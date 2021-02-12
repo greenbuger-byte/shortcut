6ecb0b892fa886dc772df033eaff2d6be98ffb12
greenbuger <iwebchester@gmail.com>
2021-02-13 04:46:43 +0500

    Починено добавление ссылки

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -6,13 +6,11 @@ module.exports = async (req, res, next) =>{
         const token = req.headers.authorization.split(" ")[1] // token
         if(!token){
           return res.status(401).json({message: "Не авторизован"});
-            
         } 
         const decoded =await jwt.verify(token, config.get("jwtSecret"));
         req.auth = decoded;
         next();
     }catch(err){
-        console.log(err);
         res.status(401).json({err});
         return;
     }
